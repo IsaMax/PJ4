1259b80d48be50dfe04f9ee19453fb911ff6f54b
maxime <maxime.isambert@gmail.com>
2019-10-16 22:32:34 +0200

    bloc gauche + navbar

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+
+  <link rel="stylesheet" href="css/pj4.css">
+</head>
+<body>
+
+    <header>
+        <nav class="nav">
+            <ul>
+                <li class="logo"> <a href="">Forteroche</a></li>
+                <li>
+                    <a href="#">Accueil</a>
+                </li>
+                <li>
+                    <a href="#">L'histoire</a>
+                </li>
+                <li>
+                    <a href="#">Biographie</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+        <section>
+
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="bloc-items-left">
+
+                            <div class="post-item">
+                                <div class="bloc-img">
+                                    <div></div>
+                                    <div class="hover-effect">
+                                        <i class=""></i>
+                                    </div>
+                                </div>
+                                <div class="bloc-text">
+                                    <p class="book-name">LIFE & STYLE , PHOTOGRAPH , TRAVEL</p>
+                                    <h3 class="bloc-title">HANGING OUT WITH OLD FRIENDS</h3>
+                                    <div class="bloc-infos">
+                                        <div class="author-part">
+                                            <i class="fa fa-user"></i>
+                                            <span>J. Forteroche</span>
+                                        </div>
+                                        <div class="date-part">
+                                            <i class="fa fa-clock-o"></i>
+                                            <span>10/10/2019</span>
+                                        </div>
+                                        <div class="comments-part">
+                                            <i class="fa fa-comments-o"></i>
+                                            <span>27</span>
+                                        </div>
+                                    </div>
+                                    <p class="bloc-teaser">Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi...</p>
+                                    <p class="bloc-button"><a href="">Découvrir</a></p>
+                                </div>
+                            </div>
+
+                            <div class="post-item">
+                                <div class="bloc-img">
+                                    <div></div>
+                                    <div class="hover-effect">
+                                        <i class=""></i>
+                                    </div>
+                                </div>
+                                <div class="bloc-text">
+                                    <p class="book-name">LIFE & STYLE , PHOTOGRAPH , TRAVEL</p>
+                                    <h3 class="bloc-title">HANGING OUT WITH OLD FRIENDS</h3>
+                                    <div class="bloc-infos">
+                                        <div class="author-part">
+                                            <i class="fa fa-user"></i>
+                                            <span>J. Forteroche</span>
+                                        </div>
+                                        <div class="date-part">
+                                            <i class="fa fa-clock-o"></i>
+                                            <span>10/10/2019</span>
+                                        </div>
+                                        <div class="comments-part">
+                                            <i class="fa fa-comments-o"></i>
+                                            <span>27</span>
+                                        </div>
+                                    </div>
+                                    <p class="bloc-teaser">Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi...</p>
+                                    <p class="bloc-button"><a href="">Découvrir</a></p>
+                                </div>
+                            </div>
+
+                            <div class="post-item">
+                                <div class="bloc-img">
+                                    <div></div>
+                                    <div class="hover-effect">
+                                        <i class=""></i>
+                                    </div>
+                                </div>
+                                <div class="bloc-text">
+                                    <p class="book-name">LIFE & STYLE , PHOTOGRAPH , TRAVEL</p>
+                                    <h3 class="bloc-title">HANGING OUT WITH OLD FRIENDS</h3>
+                                    <div class="bloc-infos">
+                                        <div class="author-part">
+                                            <i class="fa fa-user"></i>
+                                            <span>J. Forteroche</span>
+                                        </div>
+                                        <div class="date-part">
+                                            <i class="fa fa-clock-o"></i>
+                                            <span>10/10/2019</span>
+                                        </div>
+                                        <div class="comments-part">
+                                            <i class="fa fa-comments-o"></i>
+                                            <span>27</span>
+                                        </div>
+                                    </div>
+                                    <p class="bloc-teaser">Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi...</p>
+                                    <p class="bloc-button"><a href="">Découvrir</a></p>
+                                </div>
+                            </div>
+
+                            <div class="post-item">
+                                <div class="bloc-img">
+                                    <div></div>
+                                    <div class="hover-effect">
+                                        <i class=""></i>
+                                    </div>
+                                </div>
+                                <div class="bloc-text">
+                                    <p class="book-name">LIFE & STYLE , PHOTOGRAPH , TRAVEL</p>
+                                    <h3 class="bloc-title">HANGING OUT WITH OLD FRIENDS</h3>
+                                    <div class="bloc-infos">
+                                        <div class="author-part">
+                                            <i class="fa fa-user"></i>
+                                            <span>J. Forteroche</span>
+                                        </div>
+                                        <div class="date-part">
+                                            <i class="fa fa-clock-o"></i>
+                                            <span>10/10/2019</span>
+                                        </div>
+                                        <div class="comments-part">
+                                            <i class="fa fa-comments-o"></i>
+                                            <span>27</span>
+                                        </div>
+                                    </div>
+                                    <p class="bloc-teaser">Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi...</p>
+                                    <p class="bloc-button"><a href="">Découvrir</a></p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                         <div class="bloc-items-right">
+                            <div class="recent-item">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+        </section>
+
+    </main>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</body>
+</html>
