699f9525f5072d1b72cb6ca9384d636aeb2c4155
maxime <maxime.isambert@gmail.com>
2019-10-18 15:56:25 +0200

    maj3
    Finition du body hml,css

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Patrick+Hand&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  
@@ -163,13 +164,111 @@
                         </div>
                     </div>
 
+<!--  PARTIE DROITE -->
                     <div class="col-md-4">
                          <div class="bloc-items-right">
-                            <div class="recent-item">
+<!-- DERNIERS POSTS -->                             
+                            <div class="book-full">
+                                <h3>L'Histoire</h3>
+                                <div class="chapter">
+                                    <div class="thumbnail"></div>
+                                    <div>
+                                        <h4>DO NOT WASTE YOUR TIME TODAY</h4>
+                                    </div>
+                                </div>
+                                <div class="chapter">
+                                    <div class="thumbnail"></div>
+                                    <div>
+                                        <h4>Hanging Out With Old Friends</h4>
+                                    </div>
+                                </div>
+                                <div class="chapter">
+                                    <div class="thumbnail"></div>
+                                    <div>
+                                        <h4>THE MASTERS AGILITY CONTEST</h4>
+                                    </div>
+                                </div>
+                                <div class="chapter">
+                                    <div class="thumbnail"></div>
+                                    <div>
+                                        <h4>ALL PLANS WERE ALWAYS MISSES</h4>
+                                    </div>
+                                </div>
+                                <div class="chapter">
+                                    <div class="thumbnail"></div>
+                                    <div>
+                                        <h4>ALL PLANS WERE ALWAYS MISSES</h4>
+                                    </div>
+                                </div>
+                                <div class="chapter">
+                                    <div class="thumbnail"></div>
+                                    <div>
+                                        <h4>ALL PLANS WERE ALWAYS MISSES</h4>
+                                    </div>
+                                </div>
+                                
+                            </div>
+<!-- COMMENTAIRES -->
+                            <div class="comments-part-right">
+                            <h3>Commentaires récents</h3>
+                                <div class="comm">
+                                    <div class="avatar">
+                                        <img src="public/images/paque.jpg">
+                                    </div>
+                                    <div class="comms-body">
+                                        <p class="comm-author"><a href="">Max la menace</a></p>
+                                        <p class="comm-text">J'ai adoré cette introduction,</p>
+                                        <p class="comm-more"><a href="">voir &raquo;</a></p>
+                                    </div>
+                                </div>
 
+                                <div class="comm">
+                                    <div class="avatar">
+                                        <img src="public/images/paque.jpg">
+                                    </div>
+                                    <div class="comms-body">
+                                        <p class="comm-author"><a href="">Max la menace</a></p>
+                                        <p class="comm-text">J'ai adoré cette introduction,</p>
+                                        <p class="comm-more"><a href="">voir &raquo;</a></p>
+                                    </div>
+                                </div>
+
+                                <div class="comm">
+                                    <div class="avatar">
+                                        <img src="public/images/paque.jpg">
+                                    </div>
+                                    <div class="comms-body">
+                                        <p class="comm-author"><a href="">Max la menace</a></p>
+                                        <p class="comm-text">J'ai adoré cette introduction,</p>
+                                        <p class="comm-more"><a href="">voir &raquo;</a></p>
+                                    </div>
+                                </div>
+
+                                <div class="comm">
+                                    <div class="avatar">
+                                        <img src="public/images/paque.jpg">
+                                    </div>
+                                    <div class="comms-body">
+                                        <p class="comm-author"><a href="">Max la menace</a></p>
+                                        <p class="comm-text">J'ai adoré cette introduction,</p>
+                                        <p class="comm-more"><a href="">voir &raquo;</a></p>
+                                    </div>
+                                </div>
+
+                                <div class="comm">
+                                    <div class="avatar">
+                                        <img src="public/images/paque.jpg">
+                                    </div>
+                                    <div class="comms-body">
+                                        <p class="comm-author"><a href="">Max la menace</a></p>
+                                        <p class="comm-text">J'ai adoré cette introduction,</p>
+                                        <p class="comm-more"><a href="">voir &raquo;</a></p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
                 
             </div>
